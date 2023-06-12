783b7900dd5004f07589a3fdb356495a3b5820ae
jwn <naa3000.kor@gmail.com>
2023-06-12 16:54:34 +0900

    first page (1)

---
+++ lib/data.dart
@@ -0,0 +1,16 @@
+class Event {
+  String title;
+  bool done;
+  Event(this.title, this.done);
+}
+
+Map<DateTime, List<Event>> events = {
+  DateTime.utc(2023, 6, 12): [
+    Event("to do list 만들기", false),
+    Event("선형대수학 공부하기", false),
+  ],
+};
+
+List<Event> getEventsForDay(DateTime day) {
+  return events[day] ?? [];
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,91 @@
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:intl/intl.dart';
+import 'package:table_calendar/table_calendar.dart';
+import 'package:to_do_list/data.dart';
 
-void main() {
+void main() async {
+  await initializeDateFormatting();
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  DateTime selectedDay = DateTime.now();
+  DateTime focusedDay = DateTime.now();
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text("hello, world"),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
+          child: Column(
+            children: [
+              TableCalendar(
+                locale: 'ko_KR',
+                firstDay: DateTime.utc(2023, 01, 01),
+                lastDay: DateTime.utc(2023, 12, 31),
+                focusedDay: focusedDay,
+
+                // header style
+                headerStyle: HeaderStyle(
+                  titleCentered: true,
+                  titleTextFormatter: (date, locale) => DateFormat.yMMMMd(locale).format(date),
+                  formatButtonVisible: false,
+                  titleTextStyle: const TextStyle(
+                    fontSize: 18.0,
+                  ),
+                  leftChevronIcon: const Icon(
+                    Icons.arrow_left,
+                    size: 30.0,
+                  ),
+                  rightChevronIcon: const Icon(
+                    Icons.arrow_right,
+                    size: 30.0,
+                  ),
+                ),
+
+                onDaySelected: (DateTime selectedDay, DateTime focusedDay) {
+                  // 날짜가 선택되면 selectedDay와 focusDay를 갱신
+                  setState(() {
+                    this.selectedDay = selectedDay;
+                    this.focusedDay = focusedDay;
+                  });
+                },
+                selectedDayPredicate: (DateTime day) {
+                  // true를 반환하는 날짜의 모양을 변환
+                  return isSameDay(selectedDay, day);
+                },
+                eventLoader: getEventsForDay,
+              ),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: getEventsForDay(selectedDay).length + 1,
+                  itemBuilder: (context, index) {
+                    if (index != getEventsForDay(selectedDay).length) {
+                      return ListTile(
+                        title: Text(getEventsForDay(selectedDay)[index].title),
+                        subtitle: Text(getEventsForDay(selectedDay)[index].done ? "done" : "not yet"),
+                      );
+                    } else {
+                      return const ListTile(
+                        title: Text("할 일 추가하기"),
+                        subtitle: Text("할 일 추가"),
+                      );
+                    }
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
